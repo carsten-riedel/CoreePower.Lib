90429b2444537adee58e5ae02f7832087fda7944
Carsten Riedel <carstenriedel@outlook.com>
2023-03-29 22:16:18 +0200

    Added CanExecuteInDesiredScope

--- src/CoreePower.Lib/CoreePower.Lib.psm1
+++ src/CoreePower.Lib/CoreePower.Lib.psm1
@@ -7,6 +7,5 @@
 # Use Export-ModuleMember because FunctionsToExport can not export enums.
 Export-ModuleMember -Enum Scope
 Export-ModuleMember -Function HasLocalAdministratorClaim -Alias IsLocalAdministrator
+Export-ModuleMember -Function CanExecuteInDesiredScope
 Export-ModuleMember -Function NewGuid -Alias nguid
-
-
