281b2775cd80634423691ffd88d61190a87a3f12
Carsten Riedel <carstenriedel@outlook.com>
2023-04-11 17:32:19 +0200

    update

--- src/install.ps1
+++ src/install.ps1
@@ -1,4 +1,4 @@
 #Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/carsten-riedel/CoreePower.Lib/main/src/install.ps1'))
 
-Install-Module -Name CoreePower.Lib -RequiredVersion 0.0.0.29 -Scope CurrentUser -Force -AllowClobber | Out-Null
+Install-Module -Name CoreePower.Lib -Scope CurrentUser -Force -AllowClobber | Out-Null
 Initialize-CorePowerLatest
