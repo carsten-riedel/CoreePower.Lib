d03877b8a4e5df1e9282a53b75a20fe1b3a2f3eb
Carsten Riedel <carstenriedel@outlook.com>
2023-04-20 06:36:11 +0200

    Publish CoreePower.Lib 0.0.0.67

--- src/CoreePower.Lib/CoreePower.Lib.Includes.ps1
+++ src/CoreePower.Lib/CoreePower.Lib.Includes.ps1
@@ -1,3 +1,4 @@
+<#
 
 $global:includes = @(
    @{ Loaded=$false; Name="$PSScriptRoot\CoreePower.Lib.IO.ps1"} ,
@@ -16,16 +17,46 @@ for ($index = 0; $index -lt $global:includes.Count; $index++) {
         $script = Get-Content $global:includes[$index].Name -Raw
         $global:includes[$index].Loaded = $true
 
-        if ($script -ne $null)
+        if ($null -ne $script)
         {
             Write-Output "Loading $($global:includes[$index].Name)"
             # Execute the script in the global scope using the . operator and the -Scope Global parameter
             . ([scriptblock]::Create($script))
+            #Get-Command -Type Function
+            #$debug = 1
         }
     }
 }
+#>
 
 
+$includes = @(
+   @{ Loaded=$false; Name="$PSScriptRoot\CoreePower.Lib.IO.ps1"} ,
+   @{ Loaded=$false; Name="$PSScriptRoot\CoreePower.Lib.Scope.ps1"},
+   @{ Loaded=$false; Name="$PSScriptRoot\CoreePower.Lib.Initialize.ps1"},
+   @{ Loaded=$false; Name="$PSScriptRoot\CoreePower.Lib.ps1"}
+)
+
+for ($index = 0; $index -lt $includes.Count; $index++) {
+
+    $isloaded = ($includes[$index].Loaded)
+    $iscaller = ($includes[$index].Name -eq $MyInvocation.ScriptName)
+
+    if (-not($isloaded) -and -not($iscaller))
+    {
+        $script = Get-Content $includes[$index].Name -Raw
+        $includes[$index].Loaded = $true
+
+        if ($null -ne $script)
+        {
+            Write-Output "Loading $($includes[$index].Name)"
+            # Execute the script in the global scope using the . operator and the -Scope Global parameter
+            . ([scriptblock]::Create($script))
+            #Get-Command -Type Function
+            #$debug = 1
+        }
+    }
+}
 
 <#
 . "$PSScriptRoot\CoreePower.Lib.IO.ps1"
