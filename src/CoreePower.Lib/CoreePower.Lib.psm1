40692b8e4e66c760661df5535ed6c8deb6640e5b
Carsten Riedel <carstenriedel@outlook.com>
2023-05-14 11:59:59 +0200

    Publish CoreePower.Lib 0.0.0.165

--- src/CoreePower.Lib/CoreePower.Lib.psm1
+++ src/CoreePower.Lib/CoreePower.Lib.psm1
@@ -6,27 +6,3 @@
 # Calling functions directly in .psm1 files requires enhanced system configuration, which is not standard practice.
 
 . "$PSScriptRoot\CoreePower.Lib.Includes.ps1" 
-
-$selffound = Get-ModulesLocal -ModuleNames @("CoreePower.Lib") -ModulRecordState All
-
-if ($null -ne $selffound)
-{
-    $found = $false
-    foreach($item in $selffound)
-    {
-        if (Test-Path "$($item.ModuleBase)\shown.txt") {
-            $found = $true
-        }
-    }
-
-    if (-not ($found))
-    {
-        $selffound = Get-ModulesLocal -ModuleNames @("CoreePower.Lib") -ModulRecordState  Latest
-        $shown = "$($selffound.ModuleBase)\shown.txt"
-        New-Item -ItemType File -Path $shown
-        Write-Host "Thanks for installing CoreePower.Lib !"
-        Write-Host "Note: the 'Initialize-CorePowerLatest' command may conflict with existing installations. Use with caution."
-       
-    }
-}
-
--- src/install.ps1
+++ src/install.ps1
@@ -1,7 +1,7 @@
-#Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/carsten-riedel/CoreePower.Lib/main/src/install.ps1'))
+#try { Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force } catch {} ; $nugetProvider = Get-PackageProvider -ListAvailable | Where-Object Name -eq "nuget"; if (-not($nugetProvider -and $nugetProvider.Version -ge '2.8.5.201')) { $pref = $global:ProgressPreference ; $global:ProgressPreference = 'SilentlyContinue' ; Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Scope CurrentUser -Force | Out-Null ; $global:ProgressPreference = $pref; } ;[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072 ; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/carsten-riedel/CoreePower.Lib/main/src/install.ps1'))
 
 $originalProgressPreference = $global:ProgressPreference
 $global:ProgressPreference = 'SilentlyContinue'
 Install-Module -Name CoreePower.Lib -Scope CurrentUser -Force -AllowClobber | Out-Null
 $global:ProgressPreference = $originalProgressPreference
-Write-Output "CoreePower.Lib succesfully installed. Invoke Initialize-CorePowerLatest to continue the setup."
+Write-Output "Note: the 'Initialize-CorePowerLatest' command may conflict with existing installations. Use with caution."
