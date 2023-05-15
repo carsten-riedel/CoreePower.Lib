eafb5522ea3fdebdbd47b9e5fe568b817bb7e546
Carsten Riedel <carstenriedel@outlook.com>
2023-05-15 22:19:41 +0200

    Publish CoreePower.Lib 0.0.0.178

--- src/CoreePower.Lib/Initialize-CorePowerLatest.cmd
+++ src/CoreePower.Lib/Initialize-CorePowerLatest.cmd
@@ -1,3 +1,3 @@
 @echo off
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Initialize-CorePowerLatest"
-del "%~f0"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "& { Initialize-CorePowerLatest } "
+@del "%~f0"
