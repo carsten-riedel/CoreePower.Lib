bcd5b4bc2691e53448da8a4d9ecf354c17174075
Carsten Riedel <carstenriedel@outlook.com>
2023-05-18 10:58:00 +0200

    logo

--- src/CoreePower.Lib/CoreePower.Lib.Initialize.DevTools.Imagemagick.ps1
+++ src/CoreePower.Lib/CoreePower.Lib.Initialize.DevTools.Imagemagick.ps1
@@ -61,6 +61,7 @@ if ($Host.Name -match "Visual Studio Code")
     #Initialize-DevToolsImagemagick
     #https://www.svgrepo.com/
     #magick convert -density 300 -define icon:auto-resize=256,128,96,64,48,32,16 -background none sunflower-svgrepo-com.svg out.ico
-    #magick convert -background none -size 1024x1024 infile.svg outfile.png
+    #magick convert -background none -size 128x128 infile.svg outfile.png
+    #magick convert -background none -size 1280x640 "C:\base\github.com\carsten-riedel\CoreePower.Lib\src\logo.svg" "C:\base\github.com\carsten-riedel\CoreePower.Lib\src\logox.png"
 }
 
