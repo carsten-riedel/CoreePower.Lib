ee89802a2e5b2c2ae9500c49cc4775c44a94de9a
Carsten Riedel <carstenriedel@outlook.com>
2023-08-05 09:33:41 +0200

    update

--- src/install.ps1
+++ src/install.ps1
@@ -38,7 +38,7 @@ if ($parentProcess.ProcessName -eq "cmd")
 
     Copy-Item -Path "$($module.ModuleBase)\Initialize-CorePowerLatest.cmd" -Destination "$tempDirectoryPath\Initialize-CorePowerLatest.cmd"
 
-    Start-ComSpec -Arguments "/k cd ""$tempDirectoryPath"" & echo 2If you execute the command 'Initialize-CorePowerLatest', it will install the latest devtools. Please be aware that the 'Initialize-CorePowerLatest' command might potentially conflict with existing installations, so use it with caution."
+    Start-ComSpec -Arguments "/k cd ""$tempDirectoryPath"" & echo If you execute the command 'Initialize-CorePowerLatest', it will install the latest devtools. Please be aware that the 'Initialize-CorePowerLatest' command might potentially conflict with existing installations, so use it with caution."
     exit
 }
 
