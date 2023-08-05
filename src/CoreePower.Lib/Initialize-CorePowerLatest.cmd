4850730e1f525f7bb6c900f490fcd6e5a959fa61
Carsten Riedel <carstenriedel@outlook.com>
2023-08-05 08:39:40 +0200

    update installer

--- src/CoreePower.Lib/Initialize-CorePowerLatest.cmd
+++ src/CoreePower.Lib/Initialize-CorePowerLatest.cmd
@@ -2,4 +2,4 @@
 "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "& { Initialize-CorePowerLatest } "
 for /f "tokens=2*" %%A in ('reg query "HKEY_CURRENT_USER\Environment" /v "Path"') do set "output=%%B"
 SET "Path=%output%"
-@del "%~f0"
+@del "%~f0" & exit
--- src/install.ps1
+++ src/install.ps1
@@ -20,9 +20,8 @@ if ($parentProcess.ProcessName -eq "cmd")
 
     Copy-Item -Path "$($module.ModuleBase)\Initialize-CorePowerLatest.cmd" -Destination "$tempDirectoryPath\Initialize-CorePowerLatest.cmd"
 
-
-    Start-Process "$($Env:ComSpec)" -ArgumentList "/k cd ""$tempDirectoryPath"" & echo Note: the 'Initialize-CorePowerLatest' command may conflict with existing installations. Use with caution."
+    Start-Process "$($Env:ComSpec)" -ArgumentList "/k cd ""$tempDirectoryPath"" & echo If you execute the command 'Initialize-CorePowerLatest', it will install the latest devtools. Please be aware that the 'Initialize-CorePowerLatest' command might potentially conflict with existing installations, so use it with caution."
     exit
 }
 
-Write-Output "Note: the 'Initialize-CorePowerLatest' command may conflict with existing installations. Use with caution."
+Write-Output "If you execute the command 'Initialize-CorePowerLatest', it will install the latest devtools. Please be aware that the 'Initialize-CorePowerLatest' command might potentially conflict with existing installations, so use it with caution."
